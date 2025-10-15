f9b928a9c1ea5e7d00671f6f9712a3513dc033ea
IhoreQ <ihoreq@gmail.com>
2025-10-15 18:47:57 +0200

    done beach

---
+++ lang.js
@@ -0,0 +1,23 @@
+async function loadLang(lang) {
+    const res = await fetch(`lang/${lang}.json`);
+    const translations = await res.json();
+
+    document.querySelectorAll("[data-key]").forEach(el => {
+        const key = el.getAttribute("data-key");
+        el.innerHTML = translations[key] || key;
+    });
+
+    document.title = translations["page-title"];
+
+    document.querySelectorAll(".lang-switch button").forEach(b => b.classList.remove("active"));
+    document.getElementById(`${lang}-btn`).classList.add("active");
+
+    localStorage.setItem("lang", lang);
+}
+
+function changeLang(lang) {
+    loadLang(lang);
+}
+
+const savedLang = localStorage.getItem("lang") || "pl";
+loadLang(savedLang);
